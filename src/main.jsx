d31417ef75b1b4b7941f58a8ac94d99ad803354b
checkbusiness55-commits <checkbusiness55@gmail.com>
2026-08-04 20:32:49 +0000

    Add global runtime error screen catch for WebView debugging

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,7 @@
+
+window.addEventListener('error', (event) => {
+  document.body.innerHTML = '<div style="color:red;padding:20px;font-family:monospace;word-break:break-all;"><h3>Runtime Error:</h3>' + event.message + '<br><small>' + event.filename + ':' + event.lineno + '</small></div>';
+});
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
