aeb7b239fa9331f40ffe157c831831e6fe8b31d8
checkbusiness55-commits <checkbusiness55@gmail.com>
2026-08-04 20:42:33 +0000

    Fix App import reference error in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@
 window.addEventListener('error', (event) => {
   document.body.innerHTML = '<div style="color:red;padding:20px;font-family:monospace;word-break:break-all;"><h3>Runtime Error:</h3>' + event.message + '<br><small>' + event.filename + ':' + event.lineno + '</small></div>';
 });
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
